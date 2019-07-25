76a4c893cc9700dc7403c9227b0b93010069e658
ArghSee <33350988+ArghSee@users.noreply.github.com>
2019-07-25 14:19:34 -0400

    max space-fill when d.height + 1 < state.maxLevels
    addresses issue of excess whitespace when zooming in on nodes near the bottom of the hierarchy, thus improves readability of labels

--- src/sunburst.js
+++ src/sunburst.js
@@ -145,7 +145,8 @@ export default Kapsule({
     const transition = d3Transition().duration(TRANSITION_DURATION);
 
     const levelYDelta = state.layoutData[0].y1 - state.layoutData[0].y0;
-    const maxY = state.maxLevels ? Math.min(1, focusD.y0 + state.maxLevels * levelYDelta) : 1;
+    const crntMaxLvlv = state.maxLevels ? Math.min(state.maxLevels, focusD.height + 1 || state.maxLevels) : 1;
+    const maxY = state.maxLevels ? Math.min(1, focusD.y0 + crntMaxLvl * levelYDelta) : 1;
 
     // Apply zoom
     state.svg.transition(transition)
